316600c65e82bd7e99aee196ddb0eb4d0975d77e
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-02-08 09:57:41 -0500

    refactor: Improve API response handling and type validation

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,24 +5,31 @@ import { IBreadcrumbBar } from "@/interfaces/shared/IBreadcrumb";
 import { apiService } from "@/services/apiServices";
 import { ProductCard } from "@/components/products/productCard";
 import { IProduct } from "@/interfaces/schemas/IProduct";
+import { IResponse } from "@/interfaces/shared/IResponse";
 
 async function ListProductsPage() {
   const menuBreadcrumb: IBreadcrumbBar[] = [
-    {
-      name: "Inicio",
-      href: "/",
-    },
-    {
-      name: "Productos",
-      href: "#",
-    },
+    { name: "Inicio", href: "/" },
+    { name: "Productos", href: "#" },
   ];
 
-  const responseProducts = await apiService.getAll("products");
+  let products: IProduct[] = [];
+
+  try {
+    const response: IResponse = await apiService.getAll("products");
+
+    if (response.status && Array.isArray(response.response)) {
+      products = response.response as IProduct[];
+    } else {
+      console.error("Unexpected response format:", response);
+    }
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
 
   return (
     <PagesWrapper menuBreadcrumb={menuBreadcrumb}>
-      <div className="flex flex-col justify-center m-4 ">
+      <div className="flex flex-col justify-center m-4">
         <h1 className="mb-4 text-4xl font-extrabold text-center leading-none tracking-tight text-foreground md:text-5xl lg:text-6xl">
           Lista de Productos
         </h1>
@@ -35,12 +42,11 @@ async function ListProductsPage() {
             Nuevo Producto
           </Link>
         </div>
-        {responseProducts && responseProducts.status ? (
+
+        {products.length > 0 ? (
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {responseProducts.response.map((product: IProduct) => (
-              <div key={product.id}>
-                <ProductCard product={product} />
-              </div>
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
             ))}
           </div>
         ) : (
