ec52090040f6a290663f85719cb6062c96f85fee
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-02-18 22:46:39 -0500

    feat: Add state management for places and display message when no places are available

--- src/components/config/places/table-places.tsx
+++ src/components/config/places/table-places.tsx
@@ -12,13 +12,17 @@ import { Button } from "@/components/ui/button";
 import { IPlace } from "@/interfaces/shared/IPlace";
 import { useRouter } from "next/navigation";
 import { apiService } from "@/services/apiServices";
+import { useState } from "react";
 
 interface TablePlacesProps {
   places: IPlace[];
 }
 
-export default function TablePlaces({ places }: TablePlacesProps) {
+export default function TablePlaces({
+  places: initialPlaces,
+}: TablePlacesProps) {
   const router = useRouter();
+  const [places, setPlaces] = useState<IPlace[]>(initialPlaces);
 
   if (!places) return null;
 
@@ -28,19 +32,17 @@ export default function TablePlaces({ places }: TablePlacesProps) {
     const isConfirmed = confirm(
       "¿Estás seguro de que deseas eliminar este lugar?"
     );
-
     if (!isConfirmed) return;
 
     try {
       const response = await apiService.delete("Place", id);
-
       if (!response.status) {
         console.error("Error al eliminar el lugar:", response.message);
         return;
       }
 
       console.log("Lugar eliminado exitosamente.");
-      router.refresh();
+      setPlaces((prevPlaces) => prevPlaces.filter((place) => place.id !== id));
     } catch (error) {
       console.error("Error inesperado al eliminar el lugar:", error);
     }
@@ -62,50 +64,58 @@ export default function TablePlaces({ places }: TablePlacesProps) {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {places.map((place) => (
-            <TableRow key={place.id}>
-              <TableCell className="font-medium">{place.id}</TableCell>
-              <TableCell>{place.name}</TableCell>
-              <TableCell>{place.shortName}</TableCell>
-              <TableCell>
-                <span
-                  className="px-2 py-1 rounded-md text-xs font-medium text-white"
-                  style={{ backgroundColor: place.color }}
-                >
-                  {place.color}
-                </span>
-              </TableCell>
-              <TableCell>{place.enabled ? "✅" : "❌"}</TableCell>
-              <TableCell>
-                {new Date(place.createdAt).toLocaleDateString()}
-              </TableCell>
-              <TableCell>
-                {new Date(place.updatedAt).toLocaleDateString()}
-              </TableCell>
-              <TableCell className="flex gap-2">
-                <Button
-                  variant="outline"
-                  className="border border-blue-500 px-3 py-1 rounded-lg hover:bg-blue-500 transition"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    router.push(`/config/places/${place.id}/edit`);
-                  }}
-                >
-                  ✏️
-                </Button>
-                <Button
-                  variant="outline"
-                  className="border border-red-500 px-3 py-1 rounded-lg hover:bg-red-500/60 transition"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handlerRemovePlace(place.id);
-                  }}
-                >
-                  ❌
-                </Button>
+          {places.length > 0 ? (
+            places.map((place) => (
+              <TableRow key={place.id}>
+                <TableCell className="font-medium">{place.id}</TableCell>
+                <TableCell>{place.name}</TableCell>
+                <TableCell>{place.shortName}</TableCell>
+                <TableCell>
+                  <span
+                    className="px-2 py-1 rounded-md text-xs font-medium text-white"
+                    style={{ backgroundColor: place.color }}
+                  >
+                    {place.color}
+                  </span>
+                </TableCell>
+                <TableCell>{place.enabled ? "✅" : "❌"}</TableCell>
+                <TableCell>
+                  {new Date(place.createdAt).toLocaleDateString()}
+                </TableCell>
+                <TableCell>
+                  {new Date(place.updatedAt).toLocaleDateString()}
+                </TableCell>
+                <TableCell className="flex gap-2">
+                  <Button
+                    variant="outline"
+                    className="border border-blue-500 px-3 py-1 rounded-lg hover:bg-blue-500 transition"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      router.push(`/config/places/${place.id}/edit`);
+                    }}
+                  >
+                    ✏️
+                  </Button>
+                  <Button
+                    variant="outline"
+                    className="border border-red-500 px-3 py-1 rounded-lg hover:bg-red-500/60 transition"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handlerRemovePlace(place.id);
+                    }}
+                  >
+                    ❌
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={8} className="text-center">
+                No hay lugares disponibles
               </TableCell>
             </TableRow>
-          ))}
+          )}
         </TableBody>
       </Table>
     </div>
